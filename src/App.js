32b0e8251cb5dcdc6b6dd107e01a73d6513a70e3
Andy Yang <andyyang350@gmail.com>
2020-02-28 16:07:04 -0700

    redo basic layout

--- src/App.js
+++ src/App.js
@@ -6,72 +6,19 @@ import { faComments, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand >
-          <FontAwesomeIcon icon={faComments} color="#00d8ff" />
-          {" "}
-          TopChat
-          </Navbar.Brand>
-      </Navbar>
+    <div className={"wrapper"}>
+      <div className={"header"}>
+        Top
+      </div>
 
+      <div className={"main"}>
+        Middle
+      </div>
 
-      <Container>
+      <div className={"footer"}>
+        Bottom
+      </div>
 
-        <Row>
-
-          <Col>
-            <Card>
-              <Card.Header>You are user1.</Card.Header>
-              <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-
-          <Col xs lg="3">
-            <Card>
-              <Card.Header>Online Users</Card.Header>
-              <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-
-        </Row>
-      </Container>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      <Navbar fixed="bottom" bg="dark" variant="dark">
-        <FormControl
-          placeholder="Enter Message"
-          className="mr-sm-2"
-        />
-        <Button variant="secondary">
-          <FontAwesomeIcon icon={faPaperPlane} />
-        </Button>
-      </Navbar>
     </div>
   );
 }
