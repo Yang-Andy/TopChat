e6ffdc027590294c1d8a720f10318840d6618e34
Andy Yang <andyyang350@gmail.com>
2020-02-28 00:56:21 -0700

    basic ui layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Form, FormControl, Button, Row, Col, Container, Card } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComments, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand >
+          <FontAwesomeIcon icon={faComments} color="#00d8ff" />
+          {" "}
+          TopChat
+          </Navbar.Brand>
+      </Navbar>
+
+
+      <Container>
+
+        <Row>
+
+          <Col>
+            <Card>
+              <Card.Header>You are user1.</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                  With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col xs lg="3">
+            <Card>
+              <Card.Header>Online Users</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                  With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+
+        </Row>
+      </Container>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <Navbar fixed="bottom" bg="dark" variant="dark">
+        <FormControl
+          placeholder="Enter Message"
+          className="mr-sm-2"
+        />
+        <Button variant="secondary">
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </Button>
+      </Navbar>
     </div>
   );
 }
