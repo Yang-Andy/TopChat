287a582990508b744945c574141d10ed7a09fa97
Andy Yang <andyyang350@gmail.com>
2020-02-29 19:01:41 -0700

    basic layout with chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,199 @@
 import React from 'react';
 import './App.css';
-import { Navbar, Form, FormControl, Button, Row, Col, Container, Card } from 'react-bootstrap';
+import { Navbar, FormControl, Button, Card } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComments, faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className={"wrapper"}>
-      <div className={"header"}>
-        Top
-      </div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand >
+          <FontAwesomeIcon icon={faComments} color="#00d8ff" />
+          {" "}
+          TopChat
+          </Navbar.Brand>
+      </Navbar>
 
       <div className={"main"}>
-        Middle
+        <div className={"main2"}>
+
+          <Card style={{ flexGrow: 1, marginBottom: "50px" }}>
+            <Card.Header>Online Users</Card.Header>
+            <Card.Body style={{ flexGrow: 1, overflow: "auto", height: "90vh" }}>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+
+
+
+
+
+
+
+
+
+
+          <Card style={{ width: 300 }}>
+            <Card.Header>Online Users</Card.Header>
+            <Card.Body>
+              <Card.Title>Special title treatment</Card.Title>
+              <Card.Text>
+                With supporting text below as a natural lead-in to additional content.
+                </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+
+        </div>
+
       </div>
 
-      <div className={"footer"}>
-        Bottom
-      </div>
+      <Navbar fixed="bottom" bg="dark" variant="dark">
+        <FormControl
+          placeholder="Enter Message"
+          className="mr-sm-2"
+        />
+        <Button variant="secondary">
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </Button>
+      </Navbar>
 
     </div>
   );
