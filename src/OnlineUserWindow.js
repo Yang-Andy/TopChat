817312ead1108403949e7bb466ab69786f5368ee
Andy Yang <andyyang350@gmail.com>
2020-02-29 22:48:05 -0700

    change layout of online users section

--- src/OnlineUserWindow.js
+++ src/OnlineUserWindow.js
@@ -5,11 +5,13 @@ import { Button, Card } from 'react-bootstrap';
 export default class OnlineUserWindow extends React.Component {
     render() {
         return (
-            <Card style={{ width: 300 }}>
+            <Card style={{ width: 300, marginBottom: "50px" }}>
                 <Card.Header>Online Users</Card.Header>
-                <Card.Body>
-                    <Card.Title>Jimmy</Card.Title>
-                    <Card.Title>Dave</Card.Title>
+                <Card.Body className={"chatBodyContainer"}>
+                    <div className={"innerChatBodyContainer"}>
+                        <Card.Title>Jimmy</Card.Title>
+                        <Card.Title>Dave</Card.Title>
+                    </div>
                 </Card.Body>
             </Card>
         );
